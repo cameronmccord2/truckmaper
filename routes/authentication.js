05491b03434529e5045819fefb9116cfcc3bb368
Cameron McCord <cameronmccord2@gmail.com>
2013-06-23 22:08:23 -0600

    fixed authentication

--- routes/authentication.js
+++ routes/authentication.js
@@ -8,7 +8,7 @@ var sendError = function(req, res, status, message, closeAndEnd, consoleLogSpeci
 }
 
 exports.checkForToken = function(req, res, next){
-	if(req.query.token == undefined || req.query.token == null || req.query.token == ''){
+	if(req.query.token == undefined || req.query.token == null || req.query.token == '')
 		sendError(req, res, 401, 'Missing token', true);
 	else
 		next();
